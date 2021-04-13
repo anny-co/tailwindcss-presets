737738aced498ca6ebbbdf81e583a11d0ec94370
Alexander Bartolomey <occloxium@gmail.com>
2021-04-13 16:46:54 +0200

    refactor: base styles for headlines
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- base.js
+++ base.js
@@ -1,18 +1,21 @@
 const plugin = require("tailwindcss/plugin");
 
-module.exports = plugin(function ({ addBase }) {
+module.exports = plugin(function ({ addBase, theme }) {
   addBase({
     h1: {
-      fontFamily: "Gilroy",
+      fontFamily: "'Gilroy'",
       fontWeight: 600,
+      fontSize: theme('fontSize.4xl')
     },
     h2: {
-      fontFamily: "Gilroy",
+      fontFamily: "'Gilroy'",
       fontWeight: 600,
+      fontSize: theme('fontSize.3xl')
     },
     h3: {
-      fontFamily: "Gilroy",
+      fontFamily: "'Gilroy'",
       fontWeight: 600,
+      fontSize: theme('fontSize.2xl')
     },
   });
 });
