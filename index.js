16134d2747a90ccc886e35b41552ac253223dba3
Alexander Bartolomey <occloxium@gmail.com>
2021-04-13 16:33:09 +0200

    refactor: nested import to top-level config
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-const plugin = require("tailwindcss/plugin");
 const annyConfig = require("./tailwind.config");
 
 module.exports = {
-  plugins: [plugin(function ({ addBase }) {
-    addBase({
-      'h1': {
-        fontFamily: 'Gilroy',
-        fontWeight: 600,
+  darkMode: 'media', // or 'media' or 'class'
+  mode: 'jit',
+  purge: {
+    enabled: true,
+    content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Lato"],
       },
-      'h2': {
-        fontFamily: 'Gilroy',
-        fontWeight: 600,
+      colors: {
+        anny: "#333366",
+        "anny-dark": "#131333",
+        primary: "#2b6af8",
+        "primary-muted": "#c0d0f4",
+        secondary: "#ff9814",
+        "secondary-muted": "#ffc478",
+        neutral: "#ffffff",
+        "neutral-muted": "#f3f3f3",
       },
-      'h3': {
-        fontFamily: 'Gilroy',
-        fontWeight: 600,
-      },
-      
-    })
-  }, annyConfig)],
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+
+      ...colors,
+
+      gray: colors.trueGray, // set gray to trueGray palette
+    },
+  },
+  plugins: [
+    require('./base')
+  ],
 };
