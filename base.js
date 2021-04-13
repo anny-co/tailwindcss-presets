6b922eba4e6f3392edddebcf94e53dcd1d8482ad
Alexander Bartolomey <occloxium@gmail.com>
2021-04-13 16:47:55 +0200

    fix: add missing sans-serif to headlines
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- base.js
+++ base.js
@@ -3,17 +3,17 @@ const plugin = require("tailwindcss/plugin");
 module.exports = plugin(function ({ addBase, theme }) {
   addBase({
     h1: {
-      fontFamily: "'Gilroy'",
+      fontFamily: "'Gilroy', sans-serif",
       fontWeight: 600,
       fontSize: theme('fontSize.4xl')
     },
     h2: {
-      fontFamily: "'Gilroy'",
+      fontFamily: "'Gilroy', sans-serif",
       fontWeight: 600,
       fontSize: theme('fontSize.3xl')
     },
     h3: {
-      fontFamily: "'Gilroy'",
+      fontFamily: "'Gilroy', sans-serif",
       fontWeight: 600,
       fontSize: theme('fontSize.2xl')
     },
