cd74f67d4791ec809332ee4ecef34407fd9b8b6b
Alexander Bartolomey <occloxium@gmail.com>
2021-06-20 15:08:44 +0200

    deps: add eslint for formatting
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "warn",
+            2
+        ],
+        "linebreak-style": [
+            "warn",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "double"
+        ],
+        "semi": [
+            "warn",
+            "always"
+        ]
+    }
+};
