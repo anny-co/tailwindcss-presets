2102316f484650a62518fa88b705a61fd73cff22
Alexander Bartolomey <occloxium@gmail.com>
2021-06-20 16:47:45 +0200

    feat: refactor font styling from ingenium to base styles and utilities
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- base.js
+++ base.js
@@ -1,17 +1,12 @@
 const plugin = require("tailwindcss/plugin");
 
-const themeModel = {
-  anny: "#333366",
-  dark: "#131333",
-  "neutral-a": "#ffffff",
-  "neutral-b": "#f3f3f3",
-  "primary": "#2b6af8",
-  "primary-muted": "#c0d0f4",
-  "secondary": "#ff9814",
-  "secondary-muted": "#ffc478"
+const fontWeights = {
+  300: "light",
+  600: "semibold",
+  800: "extrabold",
 };
 
-module.exports = plugin(function ({ addBase, addUtilities, theme, variants }) {
+module.exports = plugin(function ({ addBase, addUtilities, addComponents, theme }) {
   addBase({
     h1: {
       fontFamily: theme("fontFamily.headline"),
@@ -27,12 +22,18 @@ module.exports = plugin(function ({ addBase, addUtilities, theme, variants }) {
     },
   });
 
-  Object.entries(themeModel).forEach(([key, defaultValue]) => addUtilities({
-    [`.bg-theme-${key}`]: {
-      backgroundColor: `var(--${key}, ${defaultValue})`
-    },
-    [`.text-theme-${key}`]: {
-      color: `var(--${key}, ${defaultValue})`
+  Object.entries(fontWeights).forEach(([weight, name]) => addUtilities({
+    [`.font-gilroy-${name}`]: {
+      fontFamily: "'Gilroy', sans-serif",
+      fontWeight: weight
     }
-  }, variants("theming")));
+  }));
+
+  addComponents({
+    ".full-page": {
+      display: "flex",
+      flexDirection: "column",
+      flexGrow: "1"
+    }
+  });
 });
