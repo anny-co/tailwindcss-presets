d0cf8f14be062bc8f18aee00a9feef579d5c9c68
Alexander Bartolomey <occloxium@gmail.com>
2021-06-20 15:09:16 +0200

    feat: add utility classes for theming both text and bg
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- base.js
+++ base.js
@@ -1,18 +1,38 @@
 const plugin = require("tailwindcss/plugin");
 
-module.exports = plugin(function ({ addBase, theme }) {
+const themeModel = {
+  anny: "#333366",
+  dark: "#131333",
+  "neutral-a": "#ffffff",
+  "neutral-b": "#f3f3f3",
+  "primary": "#2b6af8",
+  "primary-muted": "#c0d0f4",
+  "secondary": "#ff9814",
+  "secondary-muted": "#ffc478"
+};
+
+module.exports = plugin(function ({ addBase, addUtilities, theme, variants }) {
   addBase({
     h1: {
-      fontFamily: theme('fontFamily.headline'),
-      fontWeight: theme('fontWeight.semibold'),
+      fontFamily: theme("fontFamily.headline"),
+      fontWeight: theme("fontWeight.semibold"),
     },
     h2: {
-      fontFamily: theme('fontFamily.headline'),
-      fontWeight: theme('fontWeight.semibold'),
+      fontFamily: theme("fontFamily.headline"),
+      fontWeight: theme("fontWeight.semibold"),
     },
     h3: {
-      fontFamily: theme('fontFamily.headline'),
-      fontWeight: theme('fontWeight.semibold'),
+      fontFamily: theme("fontFamily.headline"),
+      fontWeight: theme("fontWeight.semibold"),
     },
   });
+
+  Object.entries(themeModel).forEach(([key, defaultValue]) => addUtilities({
+    [`.bg-theme-${key}`]: {
+      backgroundColor: `var(--${key}, ${defaultValue})`
+    },
+    [`.text-theme-${key}`]: {
+      color: `var(--${key}, ${defaultValue})`
+    }
+  }, variants("theming")));
 });
