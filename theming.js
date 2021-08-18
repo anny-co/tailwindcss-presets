f8ab3564d4b9bed3390b6b57adb391b6d524e7b0
Alexander Bartolomey <occloxium@gmail.com>
2021-08-18 12:30:06 +0200

    feat: re-add theming utilities with support for alpha on all color types
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- theming.js
+++ theming.js
@@ -1,10 +1,11 @@
 const plugin = require("tailwindcss/plugin");
+const color = require("color");
 
 const themeModel = {
-  anny: "#333366",
-  dark: "#131333",
-  "neutral-a": "#ffffff",
-  "neutral-b": "#f3f3f3",
+  "anny": "#333366",
+  "dark": "#131333",
+  "neutral": "#ffffff",
+  "neutral-muted": "#f3f3f3",
   "primary": "#2b6af8",
   "primary-muted": "#c0d0f4",
   "secondary": "#ff9814",
@@ -12,15 +13,17 @@ const themeModel = {
 };
 
 module.exports = plugin(function ({ addUtilities, variants }) {
-  Object.entries(themeModel).forEach(([key, defaultValue]) => addUtilities({
-    [`.bg-theme-${key}`]: {
-      backgroundColor: `var(--${key}, ${defaultValue})`
-    },
-    [`.text-theme-${key}`]: {
-      color: `var(--${key}, ${defaultValue})`
-    },
-    [`.border-theme-${key}`]: {
-      borderColor: `var(--${key}, ${defaultValue})`
-    }
-  }, variants("theming")));
+  Object.entries(themeModel).forEach(([key, defaultValue]) => {
+    const [r, g, b] = color(defaultValue).rgb().array();
+    return addUtilities(
+      [["bg", "backgroundColor"], ["text", "color"], ["border", "borderColor"]]
+        .map(([type, property]) => ({
+          [`.${type}-${key}`]: {
+            [`--tw-${type}-opacity`]: 1,
+            [property]: `var(--bg-${key}, rgba(${r}, ${g}, ${b}, var(--tw-${type}-opacity)))`
+          }
+        }))
+        .reduce((previous, current) => ({ ...previous, ...current }), {}),
+      variants("theming"));
+  });
 });
