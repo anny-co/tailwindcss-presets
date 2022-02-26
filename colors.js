3f76c9cc94717313cd633f3588a3e53a66e5770f
Alexander Bartolomey <occloxium@gmail.com>
2022-02-26 01:22:40 +0100

    refactor: move color definition into own file
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

---
+++ colors.js
@@ -0,0 +1,12 @@
+// anny-specific theming defaults
+// resorts to functional patterns to make colors dynamic to opacity modifiers from tailwind
+module.exports = {
+  "anny": [51, 51, 102], // "#333366"
+  "dark": [19, 19, 51], // "#131333"
+  "neutral": [255, 255, 255], // "#ffffff"
+  "neutral-muted": [243, 243, 243], // "#f3f3f3"
+  "primary": [43, 106, 248], // "#2b6af8"
+  "primary-muted": [192, 208, 244], // "#c0d0f4"
+  "secondary": [255, 152, 20], // "#ff9814"
+  "secondary-muted": [255, 196, 120] // "#ffc478"
+};
