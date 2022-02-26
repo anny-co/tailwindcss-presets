e9cf4dae50e4dcaf0bae16e4ce784295ed9bc4d7
Alexander Bartolomey <occloxium@gmail.com>
2022-02-26 01:22:54 +0100

    revert!: add theming module again
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

---
+++ theming.js
@@ -0,0 +1,16 @@
+
+module.exports = plugin(function ({ addUtilities, variants }) {
+  Object.entries(themeModel).forEach(([key, defaultValue]) => {
+    const [r, g, b] = color(defaultValue).rgb().array();
+    return addUtilities(
+      [["bg", "backgroundColor"], ["text", "color"], ["border", "borderColor"]]
+        .map(([type, property]) => ({
+          [`.${type}-${key}`]: {
+            [`--tw-${type}-opacity`]: "1",
+            [property]: `var(--${type}-${key}, rgba(${r}, ${g}, ${b}, var(--tw-${type}-opacity)))`
+          }
+        }))
+        .reduce((previous, current) => ({ ...previous, ...current }), {}),
+      variants("theming"));
+  });
+});
